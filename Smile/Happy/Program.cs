9968e75de6d5840c658a7194b5d04571acc87be9
Lera-arr <lerok-mazurenko@yandex.ru>
2022-11-11 16:47:20 +0300

    Точка отсчета 1

---
+++ Smile/Happy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("крыса\nкорова\nтигра\nзаяц\nдракон\nзмея\nлошадь\nовца\nобезьяна\nкурицы\nсобаки\nсвиньи\n");
+            Console.Write("Введите номер года");
+            x = Convert.ToInt32(Console.ReadLine());
+
+
+
+        }
+    }
+}
