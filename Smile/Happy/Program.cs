1afe5b4d0bcd98511c9b9ff6dc057b53f1828967
Lera-arr <lerok-mazurenko@yandex.ru>
2022-11-11 16:48:36 +0300

    Точка 2

--- Smile/Happy/Program.cs
+++ Smile/Happy/Program.cs
@@ -15,6 +15,47 @@ namespace Happy
             Console.Write("Введите номер года");
             x = Convert.ToInt32(Console.ReadLine());
 
+            switch (x % 12)
+            {
+                case 4:
+                    Console.WriteLine("крысы");
+                    break;
+                case 5:
+                    Console.WriteLine("корова");
+                    break;
+                case 6:
+                    Console.WriteLine("тигра");
+                    break;
+                case 7:
+                    Console.WriteLine("заяц");
+                    break;
+                case 8:
+                    Console.WriteLine("дракон");
+                    break;
+                case 9:
+                    Console.WriteLine("змея");
+                    break;
+                case 10:
+                    Console.WriteLine("лошадь");
+                    break;
+                case 11:
+                    Console.WriteLine("овца");
+                    break;
+                case 0:
+                    Console.WriteLine("обезьяна");
+                    break;
+                case 1:
+                    Console.WriteLine("курица");
+                    break;
+                case 2:
+                    Console.WriteLine("собака");
+                    break;
+                case 3:
+                    Console.WriteLine("свинья");
+                    break;
+            }
+            Console.ReadKey();
+
 
 
         }
